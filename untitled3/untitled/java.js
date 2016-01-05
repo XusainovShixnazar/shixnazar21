052b17d56f78d1e1635a38305eb2e9923c0d83a8
ASUS_PK <shih_h.uz@mail.ru>
2016-01-05 10:45:31 +0400

    Initial commit

---
+++ untitled3/untitled/java.js
@@ -0,0 +1,12 @@
+$(document).ready(
+    function(){
+        $("#bt").click(
+            function(){
+                $("ig").hide();
+            }
+        );
+
+    }
+);/**
+ * Created by ASUS_PK on 04.01.2016.
+ */
